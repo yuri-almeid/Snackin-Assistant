7cedd219602cda6aabce707bea4e1a8e984ab714
Yuri Almeida <yurilima95@gmail.com>
2021-02-02 20:11:02 -0300

    Training chat v0.1 finished

--- Mark I/training.py
+++ Mark I/training.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-#encoding: utf-8
-
-
 # Para baixar os pacotes necessários para lingua portuguesa do nltk
 # import nltk
 # nltk.download()
@@ -23,7 +20,6 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
-
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 
@@ -34,13 +30,13 @@ import tflearn
 import random
 import json
 
-print("\n\n")
+print("\n >>> Carregando intents.json")
 # Pega todos os intents cadastrados
 with open('intents.json') as json_data:
     intents = json.load(json_data)
 
 # Lista com intents
-print("Lista com intents:")
+print(" >>> Lista com intents:")
 print(intents)
 print("\n\n")
 
@@ -49,6 +45,8 @@ classes = []
 documents = []
 ignore = ['?']
 
+
+print(" >>> Iniciando tokenização de palavras")
 # Loop pelo patterns do json intents
 for intent in intents['intents']:
     for pattern in intent['patterns']:
@@ -67,7 +65,7 @@ for intent in intents['intents']:
 words = [stemmer.stem(w.lower()) for w in words if w not in ignore]
 words = sorted(list(set(words)))
 
-# remove classe duplicadas
+# remove classes duplicadas
 classes = sorted(list(set(classes)))
 
 print (len(documents), "documents")
@@ -75,4 +73,57 @@ print (len(classes), "classes", classes)
 print (len(words), "unique stemmed words", words)
 
 
-# teste
+# Cria dados de treinamento
+training = []
+output = []
+# Cria array vazio para saida
+output_empty = [0] * len(classes)
+
+# Cria o training set, conjunto de palavras para cada sentença
+for doc in documents:
+    # Inializa conjunto
+    bag = []
+    # Lista de palavras tokenizadas do pattern
+    pattern_words = doc[0]
+    # Pega a origem de cada palavra
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    # Cria o conjunto
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # saída é 1 para tag atual e 0 para outras tags
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# Embaralhando características e criando array
+random.shuffle(training)
+training = np.array(training)
+
+# Listas de treinamento
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+
+# Resetando graficos (tem b.o. aqui ainda)
+#tf.reset_default_graph()
+
+# Criação da rede neural
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Definindo modelo da rede e Tensorboard
+model = tflearn.DNN(net, tensorboard_dir='./brain/tflearn_logs')
+
+# Inicia o treinamento
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('./brain/model.tflearn')
+
+import pickle
+pickle.dump( {'words':words, 'classes':classes, 'train_x':train_x, 'train_y':train_y}, open( "./brain/training_data", "wb" ) )
+
+
+#
