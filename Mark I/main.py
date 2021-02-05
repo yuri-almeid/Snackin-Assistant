d67e0339b915c1f6489dc61f08194dd42a0dd294
Yuri Almeida <yurilima95@gmail.com>
2021-02-05 11:14:55 -0300

    Thinking processing

--- Mark I/main.py
+++ Mark I/main.py
@@ -27,4 +27,29 @@ import tflearn
 import random
 import json
 
+import pickle
+
+# Transforma o conjunto de bytes em uma hierarquia de objetos
+data = pickle.load( open( "./brain/training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+with open('./brain/intents.json') as json_data:
+    intents = json.load(json_data)
+
+
+# Recria a rede
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, 10)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Redefine o modelo
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+# Carrega o modelo salvo
+model.load('./brain/model.tflearn')
 
