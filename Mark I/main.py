b2cece6195d540313ec4d7c0c8390f29133716eb
Yuri Almeida <yurilima95@gmail.com>
2021-02-05 10:59:29 -0300

    Creation of the chat processing library

--- Mark I/main.py
+++ Mark I/main.py
@@ -6,10 +6,25 @@
 #
 
 
-# Módulos
+# Módulos de sitentização de voz
 from IBM_interface import * 
 tts = IBM_auth()
 
+# Aviso inicial
+say('Iniciando programa, por favor aguarde!', tts)
+
+# Bibliotecas para processamento de linguagem natural
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+
+# Bibliotecas do tensorflow
+import tensorflow as tf
+import numpy as np
+import tflearn
+import random
+import json
 
 
-say('Bem vindo a loja isnéquin melhor loja do mundo', tts)
