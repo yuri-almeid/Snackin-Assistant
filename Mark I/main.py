6c9b36ef1d82b2b0cc6e566f88215840e2f427e8
Yuri Almeida <yurilima95@gmail.com>
2021-02-05 11:24:11 -0300

    Add the thinking processing to the main code

--- Mark I/main.py
+++ Mark I/main.py
@@ -53,3 +53,68 @@ model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
 # Carrega o modelo salvo
 model.load('./brain/model.tflearn')
 
+
+######### Thinking processing ##########
+
+def clean_up_sentence(sentence):
+    # Tokeniza as sentenças
+    sentence_words = nltk.word_tokenize(sentence, language='portuguese')
+    # Simplifica palavras
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# returning bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # Tokeniza as sentenças
+    sentence_words = clean_up_sentence(sentence)
+    # Cria conjunto de palavras
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+context = {}
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    # Gera as probalidades para o modelo
+    results = model.predict([bow(sentence, words)])[0]
+    # Filtra a saida
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    # Organiza pela força da probabilidade da resposta correta
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # Retorna a tupla com o intent e a probabilidade 
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # set context for this intent if necessary
+                    if 'context_set' in i:
+                        if show_details: print ('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+                    # check if this intent is contextual and applies to this user's conversation
+                    if not 'context_filter' in i or \
+                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print ('tag:', i['tag'])
+                        # a random response from the intent
+                        return print(random.choice(i['responses']))
+
+            results.pop(0)
+
+
+#####################################################################
