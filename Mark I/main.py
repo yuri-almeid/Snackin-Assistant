6283ebe181e9b2e30103f0e38d90aa0e6b8950eb
Yuri Almeida <yurilima95@gmail.com>
2021-02-21 19:21:53 -0300

    date acquisition

--- Mark I/main.py
+++ Mark I/main.py
@@ -24,7 +24,35 @@ from playsound import playsound # Biblioteca para reprodução de arquivos MP3
 from tinytag import TinyTag # Biblioteca para análise de .mp3
 import time # Biblioteca de delay
 
+from datetime import datetime # módulo de tempo
+from datetime import date # Módulo de tempo (data)
+
+
 
 # Awake
-# Cria rodina que acorda > pega o nome do cliente > Pega a hora exata
-# dá boas vindas ao cliente > Falta pensar no resto
+# Cria rodina que acorda > (pegar da API)
+# pega o nome do cliente > (pegar da API) 
+# Pega a hora exata > ()
+# dá boas vindas ao cliente > 
+# Falta pensar no resto > 
+
+awake = True
+
+
+'''
+ elif cmd_type == 'asktime':
+        now = datetime.now()
+        result = 'It is ' + str(now.hour) + ' hours and ' + str(now.minute) + ' minutes.'
+        
+    # 2 Comando de data
+    elif cmd_type == 'askdate':
+        now = date.today()
+        result = str(now.strftime("Today is %B, %d, it's a %A")) + '.'
+'''
+
+if (awake	== True){
+  hour = datetime.now()
+  day = date.today()
+  print(hour)
+
+}
