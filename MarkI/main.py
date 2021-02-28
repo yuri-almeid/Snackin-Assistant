03eba20d9474d0ecb40dc3df2cbc6aa391972334
Yuri Almeida <yurilima95@gmail.com>
2021-02-28 19:37:53 -0300

    applying updates

--- MarkI/main.py
+++ MarkI/main.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
+#
 #
 #   Snackin' Assistant Mark I
 #   Author: Yuri L. Almeida
 #
 from datetime import datetime # módulo de tempo
 from datetime import date # Módulo de tempo (data)
-import requests
-#import flask
-import random
-import json
+import requests # Módulo para webrequest
+import random # Módulo Random
+import json # Modulo para lidar com arquivos JSON
 
+from fastapi import FastAPI # Módulo para o FASTAPI
+import uvicorn # Modulo para rodar o fastapi
+from pydantic import BaseModel # Criação de base de dados
 
-# Cria rodina que acorda > (pegar da API)
-# Pega o nome do cliente > (pegar da API) 
+# Cria rodina que acorda > (OK)
+# Pega o nome do cliente > (OK) 
 # Pega a hora exata > (ok)
 # Dá boas vindas ao cliente > (ok)
 # Criar log > (ok)
@@ -23,23 +25,37 @@ import json
 # Rotina: Saída > ()
 # Pensar no resto > (working on it)
 
-# Variável que define se de fato deve acordar ou não
-awake = True
+
+# Cria aplicação da API
+app = FastAPI()
+
+# Classe para modelo de requisição
+class User(BaseModel):
+  name: str
+  location: str
+
+# Banco de respostas prontas
+c_greeting = ['como vai?', 'como tem passado?', 'que bom te ver aqui!', 'fico feliz em te ver aqui',
+                'desejo boas compras!', 'te desejo uma boa experiência Snackin']
+c_weekend = ['que tal uma bebida gelada para aproveitar o seu final de semana?',
+          'desejamos um excelente final de semana!']
 
 
-
-# Se deve realizar seguinte rotina
-if awake	== True:
+@app.post("/")
+def welcome(user: User):
 
   # Cria objeto do log
   log = {'nome': '',
+          'condominio': '',
           'data': {'data': '',
                            'hora': '',
                            'dia': ''}}
-
-  # Pega o nome do cliente
-  name = 'Yuri Almeida'
+  
+  # Pega o nome e localização do cliente
+  name = user.name
   log['nome'] = name
+  loc = user.location
+  log['condominio'] = loc
 
   spc = ' ' # apenas uma variável facilitatória
 
@@ -55,10 +71,6 @@ if awake	== True:
   # Salva dia da semana
   log['data']['dia'] = day
 
-  c1 = ['como vai?', 'como tem passado?', 'que bom te ver aqui!', 'fico feliz em te ver aqui',
-                'desejo boas compras!', 'te desejo uma boa experiência Snackin']
-  c_wdn = ['que tal uma bebida gelada para aproveitar o seu final de semana?',
-          'desejamos um excelente final de semana!']
 
   # Escolha a saudação correta para o horário
   if int(now.hour) >= 4 and int(now.hour) < 12:
@@ -67,35 +79,18 @@ if awake	== True:
     greating = 'Boa tarde,'
   else:
     greating = 'Boa noite,'
-  
+
+
   # Concatena a saudação com o nome do cliente e um complemento inicial
-  phrase = greating + spc + name + spc + random.choice(c1)
+  msg = greating + spc + name + spc + random.choice(c_greeting)
 
   # Mensagem extra para final de semana (sextou)
   if day == 'Friday' or day == 'Saturday':
-    phrase = phrase + ', ' + random.choice(c_wdn)
+    msg = msg + ', ' + random.choice(c_weekend)
+
+  log['Mensagem'] = msg
+
+  return log
 
 
-
-  log['Mensagem'] = phrase
-
-
-  print(log)
-  print(phrase)
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
+# RODA COM: uvicorn main:app --reload
