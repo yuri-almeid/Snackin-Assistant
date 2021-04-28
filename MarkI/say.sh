6d5d7b0f8920f73f971ff3e7de534949aadb8f67
Yuri Almeida <yurilima95@gmail.com>
2021-04-28 16:49:54 -0300

    play audio on root fixed

--- MarkI/say.sh
+++ MarkI/say.sh
@@ -1,3 +1,4 @@
 echo Reproduzindo Voz
+pulseaudio --start --log-target=syslog
 mpg123 speech.mp3
 echo Voz Reproduzida com sucesso
--- MarkI/say_test.sh
+++ MarkI/say_test.sh
@@ -1,3 +1,4 @@
 echo Reproduzindo Voz Teste
+pulseaudio --start --log-target=syslog
 mpg123 speech_test.mp3
 echo Voz Reproduzida com sucesso
